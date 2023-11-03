4d8fccdfdc7d0fc4b2c80d3675a2cbb5f07733f8
lim211 <lim211@mcmaster.ca>
2023-11-03 15:25:35 -0400

    Added preview video for image convolution, removed placeholder text

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -29,18 +29,10 @@ const AboutMe = forwardRef<Ref, Prop>((prop, ref) => {
     >
       <Header text="About Me" />
       <div className="w-4/6">
-        hi every1 im new!!!!!!! *holds up spork* my name is katy but u can call
-        me t3h PeNgU1N oF d00m!!!!!!!! lol…as u can see im very random!!!! thats
-        why i came here, 2 meet random ppl like me ^_^… im 13 years old (im
-        mature 4 my age tho!!) i like 2 watch invader zim w/ my girlfreind (im
-        bi if u dont like it deal w/it) its our favorite tv show!!! bcuz its
-        SOOOO random!!!! shes random 2 of course but i want 2 meet more random
-        ppl =) like they say the more the merrier!!!! lol…neways i hope 2 make
-        alot of freinds here so give me lots of commentses!!!!
-        DOOOOOMMMM!!!!!!!!!!!!!!!! {"<"}--- me bein random again ^_^
-        hehe…toodles!!!!! love and waffles, t3h PeNgU1N oF d00m
+        I love coding! Recently focusing on web development with React. Check
+        out some of my skills below.
       </div>
-      <div className="lg:w-2/3 w-11/12 grid grid-cols-3 gap-2">
+      <div className="lg:w-2/3 w-11/12 grid grid-cols-3 gap-2 mt-8">
         <h3 className="text-base md:text-xl text-accent  m-auto">
           Programming Languages
         </h3>
